abd62d6b2b278c3da2187dadf65dcd4fc3af2977
mnauliady <mnauliady@gmail.com>
2023-02-15 12:00:28 +0700

    first react js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import fungsi navbar dan element
+import Navbar from "./navbar";
+import Element from "./element";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// fungsi untuk render
+const render = (content, id) => {
+  return ReactDOM.render(content, document.getElementById(id));
+};
+
+// memanggil fungsi render
+render(<Element />, "root");
+render(<Navbar />, "navbar");
